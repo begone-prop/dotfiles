c4e09eda65bb548dd30b8186839f57c7d503a472
anon <anon@anon.org>
2024-10-24 06:32:43 +0200

    add tmux and tabs plugin to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,10 +2,17 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = '0.1.8',
     priority = 1000,
-    dependencies = {"nvim-lua/plenary.nvim"},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "camgraff/telescope-tmux.nvim",
+        "LukasPietzschmann/telescope-tabs",
+    },
     config = function()
         local builtin = require("telescope.builtin")
 	    local util = require("config.util")
+
+        local ts_tmux = require('telescope').load_extension('tmux')
+        local ts_tabs = require('telescope').load_extension('telescope-tabs')
 
         util.shortcut("of", builtin.find_files)
         util.shortcut("oF", function()
@@ -21,5 +28,8 @@ return {
         util.shortcut("oC", builtin.colorscheme)
 
         util.shortcut("od", builtin.lsp_references)
+        util.shortcut("otw", function() ts_tmux.windows{} end)
+        util.shortcut("ots", function() ts_tmux.sessions{} end)
+        util.shortcut("oT", ts_tabs.list_tabs)
     end
 }
